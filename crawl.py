f8ab23996d3f5a1347d8454bba6dd8904c83d6e3
DylanSeto <dyl.seto@protonmail.com>
2023-10-30 18:59:42 -0400

    Adds comments for understanding the source code

--- crawl.py
+++ crawl.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import json
 
+# Header of the GET requests.
 def get_headers():
     return {"Accept" : "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
 	"Accept-Encoding" : "gzip, deflate, br",
@@ -11,6 +12,7 @@ def get_headers():
 	"User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36"}
             
 
+# Get the crumb token needed for calling later API calls
 url = 'https://query2.finance.yahoo.com/v1/test/getcrumb'
 headers = get_headers()
 
@@ -18,6 +20,12 @@ response = requests.get(url, headers=get_headers(), timeout=30)
 
 crumb = response.text
 
+# Yahoo finance as of October 2023, requires two seperate API calls
+# For retrieving all stock data:
+# 1) Quote: Contains most stock data, what's missing are things like the sector the company is in.
+# 2) Quote Summary: This API call contains the data missed by the first API.
+# Note: For this example, the AAPL stock is retrieved
+# in the PowerBI file, AAPL is replaced with the stock symbol which is fed through the SQL tables provided.
 url2 = 'https://query1.finance.yahoo.com/v7/finance/quote?formatted=true&crumb=' + crumb + '&lang=en-US&region=US&symbols=AAPL'
 
 response2 = requests.get(url2, headers=get_headers(), timeout=30)
@@ -26,6 +34,7 @@ response2 = requests.get(url2, headers=get_headers(), timeout=30)
 res = response2.json()
 df = pd.json_normalize(res['quoteResponse']['result'])
 
+# See Note above, AAPL stock is used as an example.
 url3 = 'https://query2.finance.yahoo.com/v10/finance/quoteSummary/AAPL?formatted=true&crumb=' + crumb + '&modules=summaryProfile'
 
 response4 = requests.get(url3, headers=get_headers(), timeout=30)
--- tables.sql
+++ tables.sql
@@ -5,7 +5,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Dividend_history](
 	[Ticker] [nchar](10) NOT NULL,
-	[Account] [nchar](10) NULL,
+	[Account] [nchar](10) NULL, /* Account type: Personal, TFSA */
 	[Amount] [money] NULL,
 	[Date] [date] NULL
 ) ON [PRIMARY]
@@ -16,9 +16,9 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 CREATE TABLE [dbo].[Stocks](
-	[Account] [nchar](50) NULL,
+	[Account] [nchar](50) NULL, /* Account type: Personal, TFSA */
 	[Ticker] [nchar](50) NULL,
 	[Shares] [float] NULL,
 	[Cost Per Share] [money] NULL,
-	[Bought Exchange Rate] [money] NULL
+	[Bought Exchange Rate] [money] NULL /* Exchange Rate of the stock, if bought in other than CAD */
 ) ON [PRIMARY]
